e36ae1121683f618cc1c1622cf085fc4e56fdc57
rencebry <clarence_borlaza@dlsu.edu.ph>
2024-11-23 16:18:45 +0800

    Update mp2_asm.asm

--- mp2_asm.asm
+++ mp2_asm.asm
@@ -4,6 +4,31 @@ default rel
 global asmaccel
 
 asmaccel:
-    mov rax, rcxd
-    sub rax, rdx
+    ; n@rcx, matrix@xmm0, results@r8
+    ; matrix - [0]=initial vel, [1]=final vel, [2]=time
+    ; compute acceleration: (Vf - Vi)/T
+    ; convert km/h to m/s
+    ; convert final answer ato int
+    L1:
+        
+        
+        
+        add r8, 4
+        loop L1
+    ret
+    
+; for reference only
+
+asmvecsub:
+    ; n@rcx, vec1@rdx, vec2@r8, vec3@r9
+    L1:
+        mov rax, [rdx]
+        mov rbx, [r8]
+        sub rax, rbx
+        mov [r9], rax
+        add rdx, 4
+        add r8, 4
+        add r9, 4
+        loop L1
+        
     ret
