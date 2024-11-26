d33129a07129e6dd3e8c46ead500fcda927a88e8
Devon Javier <devonjarekjavier@gmail.com>
2024-11-26 17:07:28 +0800

    Added execution time calculation for asmaccel

--- mp2_C.c
+++ mp2_C.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <math.h>
 
 extern int asmaccel(double vec1, double vec2, double time);
 
@@ -30,16 +32,22 @@ int main()
 	}
 
 
-	// per row asmaccel() is called 
+	clock_t exec_time;
+	exec_time = clock();
 	for(i = 0; i < n; i++) {
         results[i] = asmaccel(matrix[i][0], matrix[i][1], matrix[i][2]); 
     }
-    
+    exec_time = clock() - exec_time;
+
+	double time_taken = ((double)exec_time)/CLOCKS_PER_SEC;
+
+
 	
 	printf("Acceleration Results\n");
 	for(i = 0; i < n; i++)
 		printf("%d\n", results[i]);
 	
+	printf("\nTotal execution time of function: %.8lfs", time_taken);
 	
 	return 0;
 }
